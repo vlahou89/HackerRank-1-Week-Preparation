6871dc4ca63d26692c7bc3b85841416d4ece86d7
Margarita Vlachou <margvlahou@gmail.com>
2022-11-22 05:19:06 +0000

    Update min_max_sum.js

--- min_max_sum.js
+++ min_max_sum.js
@@ -1,20 +1,8 @@
-function plusMinus(arr) {
-    let positive = 0;
-    let negative = 0;
-    let zero = 0 ;
-    
-    for (let i=0; i < arr.length; i++){
-        if( arr[i] === 0){
-            zero++;
-        }else if (arr[i] > 0){
-            positive++;
-        }else{
-            negative++;
-        }
-    }
-    
-    console.log((positive/arr.length).toFixed(6));
-    console.log((negative/arr.length).toFixed(6));
-    console.log((zero/arr.length).toFixed(6));
-
+function miniMaxSum(arr) {
+  let sum = arr.reduce((a, b) => {
+    return a + b;
+  });
+  const min = sum - Math.max(...arr);
+  const max = sum - Math.min(...arr);
+  console.log(min + " " + max);
 }
